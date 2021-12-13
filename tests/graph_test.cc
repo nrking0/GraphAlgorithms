b5d757074c62d5ca57635e6b6789aa6f6db8508a
Jimmy Huang <jhuan35@siebl-0224-18.ews.illinois.edu>
2021-12-12 22:22:52 -0600

    Added Test BFS test case

--- tests/graph_test.cc
+++ tests/graph_test.cc
@@ -97,8 +97,73 @@ TEST_CASE("Test getTranspose") {
 }
 
 TEST_CASE("Test BFS") {
-    SECTION("Small 1") {
+    // We traversed these graphs by hand to figure out what 
+    // the correct traversals (starting from node 0) should be:
+
+    SECTION("5 nodes, WCC") {
         Graph g(5);
-        
+        g.addEdge(0,3);
+        g.addEdge(1,0);
+        g.addEdge(1,3);
+        g.addEdge(1,4);
+        g.addEdge(2,0);
+        g.addEdge(3,2);
+        g.addEdge(4,1);
+        std::vector<int> correct_traversal{0, 3, 2};
+        std::vector<int> our_traversal = g.BFS(0);
+        for (int i = 0; i < (int) our_traversal.size(); ++i) {
+            REQUIRE(our_traversal[i] == correct_traversal[i]);
+        }
+    }
+
+    SECTION("5 nodes, SCC") {
+        Graph g(5);
+        g.addEdge(0,1);
+        g.addEdge(0,3);
+        g.addEdge(1,0);
+        g.addEdge(1,3);
+        g.addEdge(1,4);
+        g.addEdge(2,0);
+        g.addEdge(3,2);
+        g.addEdge(4,1);
+        std::vector<int> correct_traversal{0, 1, 3, 4, 2};
+        std::vector<int> our_traversal = g.BFS(0);
+        for (int i = 0; i < (int) our_traversal.size(); ++i) {
+            REQUIRE(our_traversal[i] == correct_traversal[i]);
+        }
+    }
+
+    SECTION("13 nodes, disconnected") {
+        Graph g(13);
+        g.addEdge(0,2);
+        g.addEdge(0,3);
+        g.addEdge(2,6);
+        g.addEdge(3,2);
+        g.addEdge(6,2);
+        g.addEdge(6,11);
+        g.addEdge(10,2);
+        g.addEdge(10,3);
+        g.addEdge(11,0);
+
+        g.addEdge(1,5);
+        g.addEdge(1,8);
+        g.addEdge(4,7);
+        g.addEdge(5,4);
+        g.addEdge(8,9);
+        g.addEdge(9,12);
+        g.addEdge(12,1);
+        g.addEdge(12,8);
+
+        std::vector<int> correct_traversal_1{0, 2, 3, 6, 11};
+        std::vector<int> our_traversal_1 = g.BFS(0);
+        for (int i = 0; i < (int) our_traversal_1.size(); ++i) {
+            REQUIRE(our_traversal_1[i] == correct_traversal_1[i]);
+        }
+
+        std::vector<int> correct_traversal_2{1, 5, 8, 4, 9, 7, 12};
+        std::vector<int> our_traversal_2 = g.BFS(1);
+        for (int i = 0; i < (int) our_traversal_2.size(); ++i) {
+            REQUIRE(our_traversal_2[i] == correct_traversal_2[i]);
+        }
     }
 }
